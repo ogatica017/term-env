d6e40000fbf1b13dd8f633310a9fed04161e5f08
Omar Gatica <ogatica@Omars-Mac-mini.lan>
2026-03-28 13:25:41 -0700

    Load powerlevel10k theme from Homebrew

--- shell/theme.zsh
+++ shell/theme.zsh
@@ -2,6 +2,18 @@ if [[ "${TERM_ENV_DISABLE_THEME:-0}" == "1" ]]; then
   return 0
 fi
 
+if [[ -x /opt/homebrew/bin/brew ]]; then
+  BREW_PREFIX="$(/opt/homebrew/bin/brew --prefix)"
+elif [[ -x /usr/local/bin/brew ]]; then
+  BREW_PREFIX="$(/usr/local/bin/brew --prefix)"
+else
+  BREW_PREFIX=""
+fi
+
+if [[ -n "$BREW_PREFIX" && -f "$BREW_PREFIX/share/powerlevel10k/powerlevel10k.zsh-theme" ]]; then
+  source "$BREW_PREFIX/share/powerlevel10k/powerlevel10k.zsh-theme"
+fi
+
 if [[ -f "$HOME/.p10k.zsh" ]]; then
   source "$HOME/.p10k.zsh"
 fi
