83d3b4e9126032de070143e9768a2875f61109a7
Omar Gatica <ogatica@Omars-Mac-mini.local>
2026-03-29 14:14:00 -0700

    Add git aliases and fix fzf shell integration

--- shell/aliases.zsh
+++ shell/aliases.zsh
@@ -2,6 +2,8 @@
 alias ll='ls -lah'
 alias la='ls -A'
 alias l='ls -CF'
+alias hgrep='history | grep'
+alias goland='open -na "GoLand.app"'
 
 # Make helpers
 alias mcr='make compile-reqs'
@@ -11,4 +13,16 @@ alias ml='make lint'
 alias mt='make test'
 
 # Git helpers
+alias gs='git status'
+alias gca='git commit --amend --no-edit'
+alias gpr='git pull -r origin main'
+alias gsp='git show --pretty --name-only'
 alias gcm='git checkout main && git pull origin main'
+alias gch='git checkout'
+
+gchf() {
+  local branch
+  branch="$(git branch --format='%(refname:short)' | fzf)" || return
+  [[ -n "$branch" ]] || return
+  git checkout "$branch"
+}
